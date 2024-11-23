8f6ff5315cb1ca81f22f305297d5bf57a76dde89
Rahul Vaiwala <aaryan2016@yahoo.in>
2024-11-24 00:08:13 +0530

    trial with lama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import HotelSearch from "@/components/HotelSearch";
+import Navbar from "@/components/Navbar";
 import Topbar from "@/components/Topbar";
 import { Button } from "@/components/ui/button";
 import { createAllAction } from "@/server/action";
@@ -7,15 +8,18 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="bg-blue-900">
-      <div className="container mx-auto">
-        <Topbar />
-        <HeroSection />
-        <div className="bg-yellow-600">
-          <HotelSearch />
-        </div>
-      </div>
-    </div>
-
+    // <div className="bg-blue-900">
+    //   <div className="container mx-auto">
+    //     <Topbar />
+    //     <HeroSection />
+    //     <div className="bg-yellow-600">
+    //       <HotelSearch />
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,8 +4,8 @@ import { Form } from './ui/form'
 
 function HeroSection() {
     return (
-        <div>
-            <div className='flex flex-col gap-4 py-28 text-white'>
+        <div className='flex bg-blue-900 justify-center items-center'>
+            <div className='flex flex-col w-full max-w-screen-lg justify-center py-28 text-white'>
                 <h1 className='font-bold text-5xl'>Find your next stay</h1>
                 <h4 className='text-2xl'>Search deals on hotels, homes, and much more...</h4>
             </div>
