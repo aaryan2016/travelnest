6d28ff30f3bfb3d14a1a50f6dae4417e306197fe
aaryan2016 <aaryan2016@yahoo.in>
2024-12-16 22:15:01 +0530

    Revert "Frontpage lama"

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import HeaderSearch from './HeaderSearch'
+import { Input } from './ui/input'
+import { Form } from './ui/form'
 
 function HeroSection() {
     return (
-        <div className='bg-blue-900 relative'>
-            <div className='flex justify-center items-center'>
-                <div className='flex flex-col w-full max-w-screen-lg justify-center py-28 text-white'>
-                    <h1 className='font-bold text-5xl'>Find your next stay</h1>
-                    <h4 className='text-2xl'>Search deals on hotels, homes, and much more...</h4>
-                </div>
-            </div>
-
-            <div className='h-14 flex justify-center items-center absolute bottom-[-28px] w-full max-w-screen'>
-                <HeaderSearch />
+        <div>
+            <div className='flex flex-col gap-4 py-28 text-white'>
+                <h1 className='font-bold text-5xl'>Find your next stay</h1>
+                <h4 className='text-2xl'>Search deals on hotels, homes, and much more...</h4>
             </div>
         </div>
     )
